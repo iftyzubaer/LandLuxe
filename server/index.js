014db050c324fce49c04e658f2ecd8421d789ef6
Ifty Zubaer <iftyzubaer1304@gmail.com>
2024-09-01 09:16:52 +0600

    create and run the server

--- server/index.js
+++ server/index.js
@@ -1,44 +1,59 @@
-const dotenv = require("dotenv");
-const express = require("express");
-const cors = require("cors");
+import express from 'express';
+
 const app = express();
-const port = process.env.PORT || 5000;
-dotenv.config();
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri = `${process.env.MONGO}`;
-
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
+app.listen(5000, () => {
+  console.log(`Server is running on port: 5000`);
 });
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+
+
+
+
+
+// const dotenv = require("dotenv");
+// const express = require("express");
+// const cors = require("cors");
+// const app = express();
+// const port = process.env.PORT || 5000;
+// dotenv.config();
+
+// const { MongoClient, ServerApiVersion } = require("mongodb");
+// const uri = `${process.env.MONGO}`;
+
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   },
+// });
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log(
+//       "Pinged your deployment. You successfully connected to MongoDB!"
+//     );
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
 
 //middleware
-app.use(cors());
-app.use(express.json());
+// app.use(cors());
+// app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Server is running");
-});
+// app.get("/", (req, res) => {
+//   res.send("Server is running");
+// });
+// app.get("/test", (req, res) => {
+//     res.send("this is test site");
+//   });
 
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server is running on port: ${port}`);
+// });
